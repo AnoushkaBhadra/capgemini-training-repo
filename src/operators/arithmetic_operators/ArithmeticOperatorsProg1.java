5e36f2119d87bd5693a917421367db2b2c298d65
AstraeaMavi <anoushkabhadra@gmail.com>
2026-01-08 16:11:45 +0530

    included 3 more files

---
+++ src/operators/arithmetic_operators/ArithmeticOperatorsProg1.java
@@ -0,0 +1,20 @@
+package operators.arithmetic_operators;
+
+public class ArithmeticOperatorsProg1 {
+	public static void main(String []  arg) {
+		int a = 10;
+		int b = 30;
+		System.out.println(10+20);
+		System.out.println(a+b);
+		
+		System.out.println(""+10+10);
+		System.out.println(10+20+"Hello"+100);
+		System.out.println(10*30/2);
+		System.out.println('A'+'B');
+		
+		char c = 'A'+'B'; //131 
+		System.out.println(c);
+		
+	}
+	
+}
---
+++ src/operators/arithmetic_operators/ArithmeticOperatorsProg2.java
@@ -0,0 +1,12 @@
+package operators.arithmetic_operators;
+
+public class ArithmeticOperatorsProg2 {
+
+		public static void main(String args[]) {
+
+			for (int i = 1; i<128; i++) {
+				System.out.println((char)i+" "+i);
+		}
+	}
+
+}
