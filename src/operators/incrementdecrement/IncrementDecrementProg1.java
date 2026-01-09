7bf336013ab10e34e3f6260ce8e8c53bd71b8b13
AstraeaMavi <anoushkabhadra@gmail.com>
2026-01-09 11:13:11 +0530

    Incremental and Decremental Operators

--- src/operators/incrementdecrement/IncrementDecrementProg1.java
+++ src/operators/incrementdecrement/IncrementDecrementProg1.java
@@ -3,7 +3,10 @@ package operators.incrementdecrement;
 public class IncrementDecrementProg1 {
 	public static void main(String args[]) {
 		int a = 10;
-		System.out.println(++a);
-		System.out.println(a++);
+		System.out.println("Before pre-incrementation:" +a);
+		System.out.println("Pre-Incrementing: " +(++a));
+		System.out.println("After Pre-incrementing/Before Post-incrementing: " +a);
+		System.out.println("Post Incrementing: " +(a++));
+		System.out.println("After Post-Incrementing: " +a);
 	}
 }
---
+++ src/operators/incrementdecrement/IncrementDecrementProg3.java
@@ -0,0 +1,12 @@
+package operators.incrementdecrement;
+
+public class IncrementDecrementProg3 {
+	public static void main(String args[]) {
+		int a = 3; 
+		int b = ++a + a++;
+		int c = b++ + ++a + b++ + a++;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
