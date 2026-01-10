8b1d54ecb4c629983a2db4f0b3ed12743b6fa32e
AstraeaMavi <anoushkabhadra@gmail.com>
2026-01-10 20:21:59 +0530

    Uploading files

---
+++ src/operators/arithmetic_operators/DemoProg.java
@@ -0,0 +1,16 @@
+package operators.arithmetic_operators;
+
+public class DemoProg {
+	public static void main(String args[]) {
+		for(int i = 0; i < 3; i++)
+		    for(int j = 0; j < 3; j++)
+		        if(i == j) 
+		              break;
+		        else System.out.print(i + "" + j + " ");
+
+		}
+		
+				
+	}
+
+
