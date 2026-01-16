936eaaea382f3280da171db256c31fce4f582d1b
AstraeaMavi <anoushkabhadra@gmail.com>
2026-01-16 09:31:31 +0530

    Uploading assignment files

--- src/assignments/EmailAdressProg.java
+++ src/assignments/EmailAdressProg.java
@@ -1,16 +1,29 @@
 package assignments;
 
 public class EmailAdressProg {
-	public static void main(String args[]) {
-		//example address: javalang@gmail.com
-		//to check for '@', '.'
-		//valid special characters ".", "_"
-		//valid domain: yahoo/gmail/bing/protonmail
-		
-		String s = "javalang@gmail.com";
-		for(int i = 0; i<s.length(); i++) {
-			
+	
+	public static boolean checkEmailAddress(String s) {
+		if(s == null || s.contains(" ")) {
+			return false;
 		}
+		int count= s.length()-s.replace("@", "").length();
+		if (count!=1)
+		{
+			return false;
+			}
+		String regex = "^[a-zA-Z0-9._]+@(gmail\\.com||yahoo\\.com||hotmail\\.com||outlook\\.com)$";
+		
+		return s.matches(regex);
+		
 	}
+	public static void main(String args[]) {
+		
+		System.out.println(checkEmailAddress("javalang@gmail.com"));
+		System.out.println(checkEmailAddress("javalang@@gmail.com"));
+		System.out.println(checkEmailAddress("javalang @gmail.com"));
+		System.out.println(checkEmailAddress("javalange!@gmail.com "));
+	}
+
+
 
 }
